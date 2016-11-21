9d6b75b0600ae464349bc5c33c7c8f1320f318e4
mozilla <luke.pacholski@gmail.com>
2016-11-21 11:22:50 -0800

    Redid font in icomoon and added Thimble icons

--- js/icon-list.js
+++ js/icon-list.js
@@ -1,52 +1,113 @@
-$(document).ready(function(){
+var icons = [
+
+// Footer Icons
+"speech-bubble.svg",
+"github.svg",
+"question-mark.svg",
+"checkmark-circle.svg",
+"twitter.svg",
+"lock.svg",
+"heart.svg",
+"facebook.svg",
+"creative-commons.svg",
+"creative-commons-solid.svg",
+"email.svg",
+"info-i.svg",
+"community.svg",
+
+//Thimble & UI
+"trash-can.svg",
+"inspector-arrow.svg",
+"plus.svg",
+"minus.svg",
+"triangle-left.svg",
+"triangle-right.svg",
+"share.svg",
+"moon.svg",
+"sun.svg",
+"checkmark.svg",
+"document-page.svg",
+"list-items.svg",
+"pencil.svg",
+"undo.svg",
+"redo.svg",
+"sign-out.svg",
+"refresh.svg",
+"mobile-phone.svg",
+"monitor.svg",
+
+//Misc UI
+"arrow-down.svg",
+"hamburger-menu-close.svg",
+"hamburger-menu.svg",
+"clock.svg",
+"map-marker.svg",
+"checkmark.svg",
+"bookmark-small.svg",
+"bookmark.svg",
+
+
+//Logos
+"mozfest-logo.svg",
+"mozilla-logo.svg",
+
+//Skills
+"skill-collaboration.svg",
+"skill-communication.svg",
+"skill-creativity.svg",
+"skill-participate.svg",
+"skill-problem-solving.svg",
+"skill-read.svg",
+"skill-write.svg",
+
+// Science
+"access.svg",
+"code-of-conduct.svg",
+"holding-doc.svg",
+"calendar.svg",
+"flag-bent.svg",
+"open-book.svg",
+"flowerhands.svg",
+"certificate.svg",
+"pencil-hand.svg",
+"science-drop-double.svg",
+"science-drop.svg",
+
+
+// Learning Site Navigation
+"lunchbox.svg",
+"weblit-map.svg",
+"page-with-bookmark.svg",
+"three-persons.svg",
+"book.svg",
+
+
+// Content List Icons
+"ribbon.svg",  
+"airplane.svg",  
+"flag.svg",
+"institution.svg",
+
+
+
+//Learning Site Icons
+"circle-clubs.svg",
+"circle-tools.svg",
+"brain.svg",
+"connected-people.svg",
+"envelope-outline.svg",
+"learning-about-us.svg",
+"quesion-mark-outline.svg",
+"speech-bubbles.svg",
+"twitter-outline.svg",
   
-  for(var i = 0; i < icons.length; i++) {
-    
-    var iconfile = icons[i];
 
-    var iconItem = $("<div class='icon-item'></div>");
-    var iconImage = $("<img src='./svgs/" + iconfile + "'/>");
-    var iconName = $("<div class='icon-name'>" + iconfile + "</div>");
+// Learning site main CTAs
+"globe.svg",
+"bookmark-large-flat.svg",
+"envelope-letter.svg",
 
 
-    var iconClassName = ".icon-" + iconfile.replace(".svg","");
-    var iconClass = $("<div class='icon-class'>" + iconClassName + "</div>");
-
-    iconItem.attr("name",iconfile.replace(".svg",""));
-    iconItem.append(iconImage).append(iconName).append(iconClass);
-
-    // var iconTest = $("<div class='test " + iconClassName.replace(".","") + "'/>");
-    // iconItem.append(iconTest);
 
 
-    $(".dynamic-icon-list").append(iconItem);
-  }
-
-  // Search for icons when someone types
-  $(".search-ui input").on("keyup",function(){
-    var searchTerm = $(this).val();    
-    searchIcons(searchTerm);
-  })
-
-});
-
-// Searches for icons that have the term in their name
-function searchIcons(term){
-
-  $(".icon-item").show();
-
-  if(term.length == 0) {
-    return;
-  }
-
-  term = term.trim();
-  term = term.toLowerCase();
-  
-  $(".icon-item").each(function(){
-    var iconName = $(this).attr("name");
-    var index = iconName.indexOf(term);
-    if(index < 0) {
-      $(this).hide();
-    } 
-  });
-}
+]
